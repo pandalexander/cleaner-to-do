6fd5efb3fb0369406a0f0480b85b16f8e4e8749a
Alex Seeley <alexseeley134@gmail.com>
2025-01-08 10:12:26 -0700

    Add spacing to nav and mobile transform

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   return (
     <>
       <div className="min-h-screen flex flex-col ">
-        <header className="bg-white text-coolerTwo " data-testid="header-menu">
+        <header
+          className="bg-white text-coolerTwo px-spacing-2 "
+          data-testid="header-menu"
+        >
           <nav className="max-w-5xl mx-auto">
             <div className="flex justify-between py-spacing-1 items-center">
               <div className="max-w-[150px] ">
@@ -15,21 +18,32 @@ function App() {
                   <img src={myLogo} alt="" />
                 </Link>
               </div>
-              <div className="flex justify-between items-center gap-x-spacing-7">
-                <Link
-                  to="about"
-                  className="text-lg text-coolerFive hover:text-coolerTwo"
-                >
-                  Services and prices
-                </Link>
-                <Link
-                  to="about"
-                  className="text-lg text-coolerFive hover:text-coolerTwo"
-                >
-                  About
-                </Link>
+              <div className="w-full md:hidden">
+                <h1>i am hidden</h1>
               </div>
-              <div>
+              <div className="hidden md:flex justify-between items-center gap-x-spacing-7">
+                <div className="flex justify-between items-center gap-x-spacing-7">
+                  <Link
+                    to="home"
+                    className="text-lg text-coolerFive hover:text-coolerTwo"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    to="about"
+                    className="text-lg text-coolerFive hover:text-coolerTwo"
+                  >
+                    Services and prices
+                  </Link>
+                  <Link
+                    to="about"
+                    className="text-lg text-coolerFive hover:text-coolerTwo"
+                  >
+                    About
+                  </Link>
+                </div>
+              </div>
+              <div className="hidden md:block">
                 <Button
                   asChild
                   className="px-spacing-4 py-spacing-2 rounded-full bg-coolerFive hover:bg-coolerTwo hover:text-black"
