7927e18f9228c486f006757434c07a6ee6e17b03
Alex Seeley <alexseeley134@gmail.com>
2025-01-08 11:31:00 -0700

    Add hamburger menu on click

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,15 @@ import { Link, Outlet } from "react-router-dom";
 import myLogo from "./assets/logo-transparent.png";
 import { Button } from "@/components/ui/button";
 import menuIcon from "./assets/menu.svg";
+import { useState } from "react";
 
 function App() {
+  const [hamburgerClicked, setHamburgerClicked] = useState(false);
+
+  const handleHamburgerClick = () => {
+    console.log("clicked!");
+    setHamburgerClicked(!hamburgerClicked);
+  };
   return (
     <>
       <div className="min-h-screen flex flex-col ">
@@ -19,10 +26,51 @@ function App() {
                   <img src={myLogo} alt="" />
                 </Link>
               </div>
+
               <div className="md:hidden mr-spacing-4">
-                <Link to="about">
-                  <img src={menuIcon} alt="" className="min-w-[35px]" />
-                </Link>
+                <div>
+                  <img
+                    src={menuIcon}
+                    alt=""
+                    className="min-w-[35px] ml-auto"
+                    onClick={handleHamburgerClick}
+                  />
+
+                  <div
+                    className={
+                      "bg-coolerFive max-w-[250px] menu " +
+                      (hamburgerClicked ? "" : "hidden")
+                    }
+                  >
+                    {" "}
+                    <div className="flex flex-col ">
+                      <Button
+                        asChild
+                        className="flex-1 p-spacing-3 rounded-none font-normal bg-coolerFive text-coolerOne hover:bg-coolerOne hover:text-coolerThree"
+                      >
+                        <Link to="about">Home</Link>
+                      </Button>
+                      <Button
+                        asChild
+                        className="flex-1 p-spacing-3 rounded-none font-normal bg-coolerFive text-coolerOne hover:bg-coolerOne hover:text-coolerThree"
+                      >
+                        <Link to="about">Services and prices</Link>
+                      </Button>
+                      <Button
+                        asChild
+                        className="flex-1 p-spacing-3 rounded-none font-normal bg-coolerFive text-coolerOne hover:bg-coolerOne hover:text-coolerThree"
+                      >
+                        <Link to="about">About</Link>
+                      </Button>
+                      <Button
+                        asChild
+                        className="flex-1 p-spacing-3 rounded-none font-normal bg-coolerFive text-coolerOne hover:bg-coolerOne hover:text-coolerThree"
+                      >
+                        <Link to="about">Schedule a cleaning</Link>
+                      </Button>
+                    </div>
+                  </div>
+                </div>
               </div>
 
               <div className="hidden md:flex justify-between items-center gap-x-spacing-7">
@@ -50,7 +98,7 @@ function App() {
               <div className="hidden md:block">
                 <Button
                   asChild
-                  className="font-normal px-spacing-4 py-spacing-2 rounded-full bg-coolerFive hover:bg-coolerOne hover:text-coolerThree"
+                  className="font-normal px-spacing-4 py-spacing-2 rounded-full bg-coolerFive hover:bg-coolerOne text-coolerOne  hover:text-coolerThree"
                 >
                   <Link to="about">Schedule a cleaning</Link>
                 </Button>
